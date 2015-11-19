b2624170a26ae4a11f562446e99582039a6be164
Dave Gamberg <dgamberg@gmail.com>
2015-11-18 21:12:55 -0600

    initial commmit

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 // Routes //
 var index = require('./routes/index');
@@ -15,6 +16,14 @@ app.use(bodyParser.urlencoded({expanded: true}));
 app.use('/', index);
 
 // Mongo Connection //
+var mongoURI = "mongodb://localhost:27017/users_passport_session";
+var mongoDB = mongoose.connect(mongoURI).connection;
+mongoDB.on('error', function(err){
+      if(err)console.log("Mongo Error", err);
+});
+mongoDB.once('open', function(){
+   console.log("Connected to Mongo");
+});
 
 
 // Listen //
